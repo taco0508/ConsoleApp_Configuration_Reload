6a9fa5fcab0adf8391911720ddbfff62afcb1a79
Taco Chang <TacoChang@WorldFamilyEnglish.com>
2022-04-15 18:56:32 +0800

    新增問題註解

--- ConsoleApp_Configuration_Reload/Program.cs
+++ ConsoleApp_Configuration_Reload/Program.cs
@@ -2,6 +2,12 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+/*
+程式直接執行，一開始直接印出連線字串與ProjectId，在第二次執行DoSometing之前，
+將修改的json，直接覆蓋 ConsoleApp_Configuration_Reload\ConsoleApp_Configuration_Reload\bin\Debug\net6.0\appsettings.json
+再次印出的連線字串與ProjectId，只有連線字串使修改過的，ProjectId卻還是舊的...
+*/
+
 var serviceProvider = new ServiceCollection()
                       .AddTransient<IConfiguration>(sp =>
                       {
@@ -20,9 +26,30 @@ var serviceProvider = new ServiceCollection()
 
 var myService = serviceProvider.GetRequiredService<IMyService>();
 
+//迴圈執行3次
 for (int i = 0; i < 3; i++)
 {
     myService.DoSometing();
 
-    Thread.Sleep(8000);
+    Thread.Sleep(8000);//暫停8秒
 }
+
+/*
+修改的json:
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },
+  "AllowedHosts": "*",
+  "ConnectionStrings": {
+    "MyDbConnStr": "AAAAA"
+  },
+  "AppSettings": {
+      "ProjectId": "BBBB"
+  }
+}
+*/
